6c3389dd25561c2999edfc3b4058fcb1d8023aac
broadroad <tjbroadroad@163.com>
2017-08-13 12:25:37 +0800

    add weui tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+<div class="weui_tab">
+  <div class="weui-tab__panel">
+    <div id="app">
+        <router-view></router-view>
+    </div>
+  </div>
+  <div class="weui_tabbar">
+        <a href="" class="weui_tabbar_item weui_bar_item_on">
+          <div class="weui_tabbar_icon">
+            <img src="" alt="">
+          </div>
+          <p class="weui_tabbar_label">1</p>
+        </a>
+        <a href="" class="weui_tabbar_item">
+          <div class="weui_tabbar_icon">
+            <img src="" alt="">
+          </div>
+          <p class="weui_tabbar_label">2</p>
+        </a>
+        <a href="" class="weui_tabbar_item">
+          <div class="weui_tabbar_icon">
+            <img src="" alt="">
+          </div>
+          <p class="weui_tabbar_label">3</p>
+        </a>
+        <a href="" class="weui_tabbar_item">
+          <div class="weui_tabbar_icon">
+            <img src="" alt="">
+          </div>
+          <p class="weui_tabbar_label">4</p>
+        </a>
+      </div>
   </div>
 </template>
 
@@ -17,5 +47,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.weui_tabbar {
+  position: fixed;
 }
 </style>
